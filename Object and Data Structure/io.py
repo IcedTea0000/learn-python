54b91ce23ddbd49d825d5c6f4130e8e986b96bc1
IcedTea0000 <ngohoanglam65@gmail.com>
2020-12-07 15:34:18 +0700

    learn python

---
+++ Object and Data Structure/io.py
@@ -0,0 +1,22 @@
+import sys
+
+test_file = open('C:\workspace\learnPython\Object and Data Structure\/test_file.txt')
+
+# print(test_file.read())
+# print(test_file.seek(0))
+# print(test_file.read())
+# print(sys.getsizeof(test_file))
+
+# print(test_file.readlines())
+
+# print(test_file.readline())
+# print(test_file.readline())
+
+# with open('C:\workspace\learnPython\Object and Data Structure\/test_file.txt', mode='r') as new_file:
+#     for line in new_file:
+#         print('Content \n', line)
+#         print('File size: ', sys.getsizeof(line))
+
+# with open('C:\workspace\learnPython\Object and Data Structure\/test_file.txt', mode='r') as new_file:
+#       print(new_file.readline())
+
