6451eef5f80f1dba39be6fd3d1c042a59f4d47e9
Lam Ngo <ngohoanglam65@gmail.com>
2020-12-07 22:55:50 +0700

    modified io

--- Object and Data Structure/io.py
+++ Object and Data Structure/io.py
@@ -1,7 +1,8 @@
 import sys
 
-test_file = open('C:\workspace\learnPython\Object and Data Structure\/test_file.txt')
+test_file = 'C:\\Remy Martin\\workspace\\learn-python\\Object and Data Structure\\test_file.txt'
 
+# test_file = open('C:\\Remy Martin\\workspace\\learn-python\\Object and Data Structure\\test_file.txt')
 # print(test_file.read())
 # print(test_file.seek(0))
 # print(test_file.read())
@@ -17,6 +18,6 @@ test_file = open('C:\workspace\learnPython\Object and Data Structure\/test_file.
 #         print('Content \n', line)
 #         print('File size: ', sys.getsizeof(line))
 
-# with open('C:\workspace\learnPython\Object and Data Structure\/test_file.txt', mode='r') as new_file:
-#       print(new_file.readline())
+with open(test_file, mode='r') as new_file:
+      print(new_file.readline())
 
