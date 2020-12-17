91d451cc7bc69e211f0b7ea9c6a5295f22fe293d
IcedTea0000 <ngohoanglam65@gmail.com>
2020-12-17 19:12:31 +0700

    add data sandbox

---
+++ ObjectAndDataStructure/exceptions.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+
+    try:
+        result = 10 + 20
+        print(result)
+    except:
+        print('error')
+    else:
+        print('no error')
+    print('after try except block')
